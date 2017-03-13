5d299d9d586c260ff3c89ee59cd540620870aa8c
Craig J Smith <nctiggy@gmail.com>
2017-03-13 11:18:05 -0700

    porting functionality to the module

--- library/dellemc_unity_ntp.py
+++ library/dellemc_unity_ntp.py
@@ -8,26 +8,35 @@ from ansible.module_utils.urls import open_url
 class Unity(object):
 
     def __init__(self, module):
-        self.module         = module
-        self.state          = module.params['state']
-        self.force          = module.params['force']
-        self.ntp_server     = module.params['ntp_server']
-        self.date_time      = module.params['date_time']
+        self.module = module
+        self.state = module.params['state']
+        self.force = module.params['force']
+        self.ntp_server = module.params['ntp_server']
+        self.date_time = module.params['date_time']
         self.unity_hostname = module.params['unity_hostname']
         self.unity_username = module.params['unity_username']
         self.unity_password = module.params['unity_password']
-        self.base           = "https://" + self.unity_hostname + "/api"
+        self.base_url = "https://" + self.unity_hostname + "/api"
+        self.headers = {'X-EMC-REST-CLIENT': 'true',
+                        'content-type': 'application/json',
+                        'Accept': 'application/json'}
 
     def __doPost(self, url, args):
         r = open_url(url, data=args, headers=self.headers, method="POST",
                      validate_certs=False)
         return r.status_code, r.text
 
-    def __doGet(self, url, params):
+    def __doGet(self, url):
         r = open_url(url, method="GET", url_username=self.unity_username,
                      url_password=self.unity_password, headers=self.headers,
                      validate_certs=False)
         return r.status_code, r.text
+
+    def start(self):
+        url = self.base_url + "/instances/system/0"
+        r = self.__doGet(url)
+        # self.headers['EMC-CSRF-TOKEN']=r.headers['EMC-CSRF-TOKEN']
+        return r
 
 
 def main():
@@ -47,6 +56,8 @@ def main():
     )
 
     unity = Unity(module)
+    result = unity.start
+    module.exit_json(changed=True, msg=result)
 
 
 if __name__ == '__main__':
